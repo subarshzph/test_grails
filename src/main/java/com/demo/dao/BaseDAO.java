12eb9f8f0616f400ff3dd488068281da489be007
subarshzph <subarsh.thapa@zakipoint.com>
2021-07-26 18:26:30 +0545

    BaseDAO url generator added

--- src/main/java/com/demo/dao/BaseDAO.java
+++ src/main/java/com/demo/dao/BaseDAO.java
@@ -4,5 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class BaseDAO {
-
+    public String getURL(String URL) {
+        return URL.substring(0, 4).replace("http","https");
+    }
 }
