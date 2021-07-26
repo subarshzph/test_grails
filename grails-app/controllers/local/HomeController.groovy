8534728b9cfb3194d786cab714e139cfd9ac4876
subarshzph <subarsh.thapa@zakipoint.com>
2021-07-26 10:56:07 +0545

    Changes done after reset

--- grails-app/controllers/local/HomeController.groovy
+++ grails-app/controllers/local/HomeController.groovy
@@ -2,6 +2,8 @@ package local
 
 import com.demo.dao.IHome
 import grails.converters.JSON
+import org.grails.web.json.JSONArray
+import org.grails.web.json.JSONObject
 import org.springframework.beans.factory.annotation.Autowired
 
 class HomeController {
@@ -10,6 +12,12 @@ class HomeController {
 
     def index() {
         def test_data = JSON.parse(iHome.findName())
+
+        JSONObject root_json = new JSONObject()
+        JSONArray json_array = new JSONArray()
+
+        root_json.put("data", json_array)
+
         println test_data: test_data
 
         render(view: 'index', model: [test_data: test_data])
