12d1afa809f25568b0d0876a3392c54575bebf7c
subarshzph <subarsh.thapa@zakipoint.com>
2021-07-26 18:44:01 +0545

    Revert "Base dao implemented in home dao layer"
    This reverts commit dfd4d51119a12db36be906726a459c5ab87164b7.

--- src/main/java/com/demo/dao/HomeDao.java
+++ src/main/java/com/demo/dao/HomeDao.java
@@ -9,9 +9,7 @@ import org.springframework.jdbc.core.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class HomeDao extends BaseDAO implements IHome{
-
-    BaseDAO baseDAO;
+public class HomeDao implements IHome{
 
     @Autowired
     protected JdbcTemplate mainDataSourceTemplate;
@@ -39,10 +37,4 @@ public class HomeDao extends BaseDAO implements IHome{
         root_json.put("data", json_array);
         return String.valueOf(root_json);
     }
-
-    public String fetchURL(String url) {
-        return baseDAO.getURL(url);
-    }
-
-
 }
